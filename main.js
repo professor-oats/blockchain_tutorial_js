7d7a93e13e8959bae8d1f36cd3e76a24dbf9fe52
professor-oats <chronophob@gmail.com>
2021-11-01 22:52:23 +0200

    proof-of-work

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
+// We will also need to add a proof-of-work so you just can't create new blockchain on the go and make a remade version of another blockchain
+
+// We will have to set a 'difficulty' so the coin will take a while for a processor to be computed. Bitcoins' aim is to have a 10 minutes duration for every creation of a new block.
+
+// Proof-of-work is therefore a measurement of computing power to make a block
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
 	this.index = index;
 	this.timestamp = timestamp;
 	this.data = data;
 	this.previousHash = previousHash;
-	this.hash = '';
+	this.hash = this.calculateHash();
+	this.nonce = 0;
     }
 
     calculateHash() {
-	var hash1 = CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+	var hash1 = CryptoJS.SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data + this.nonce)).toString();
 	return hash1;
+    }
+
+    mineBlock(difficulty) {    // Mining is proof-of-work
+	while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {    // Take the first 'difficuly amount' of characters of our hash and make 0:s of it;
+	    this.nonce++;
+	    this.hash = this.calculateHash();
+	}
+
+	console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
 	this.chain = [this.createGenesisBlock()];
+	this.difficulty = 4;
     }
 
     createGenesisBlock() {
@@ -28,7 +45,7 @@ class Blockchain {
 
     addBlock(newBlock) {
 	newBlock.previousHash = this.getLatestBlock().hash;    // When adding new block the previous hash must be the same as the hash of the latest block for the new block to be valid
-	newBlock.hash = newBlock.calculateHash();    // Create a hash for this block
+	newBlock.mineBlock(this.difficulty);    // Create a hash for this block
 	this.chain.push(newBlock);
     }
 
@@ -52,15 +69,10 @@ class Blockchain {
 }
 
 let savjeeCoin = new Blockchain();
+
+console.log('Mining block 1...');
 savjeeCoin.addBlock(new Block(1, "10/11/2021", { amount: 4 }));
+console.log('Mining block 2...');
 savjeeCoin.addBlock(new Block(2, "12/11/2021", { amount: 10 }));
 
-console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
-
-savjeeCoin.chain[1].data = { amount: 100 };    // Try to tamper with the data changing the amount sent at chain position 1
-savjeeCoin[1].hash = savjeeCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
-
-//console.log(JSON.stringify(savjeeCoin, null, 4));
 
